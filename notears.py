174cd185d83d02b15cc5a1e028b16adb72a1af69
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2020-05-21 10:08:45 -0300

    finished function

--- notears.py
+++ notears.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy.linalg as slin
-import tensorflow as tf
-import tensorflow_probability as tfp 
+import scipy.optimize as sopt
 
 def notears_linear(X, h_tol, threshold, lambda1, c = 0.25, rho_max = 1e16, max_iter = 100):
     '''
@@ -18,16 +17,14 @@ def notears_linear(X, h_tol, threshold, lambda1, c = 0.25, rho_max = 1e16, max_i
     Outputs:
         W_est (numpy.matrix): [n_variables, n_variables] estimated graph
     '''
+
+    def _adj(W):
+        '''Transforms an array [2 * n^2] to matrix [n, n]'''
+        return (W[:n_variables*n_variables] - W[n_variables*n_variables:]).reshape([n_variables,n_variables])
+
     def _h(W):
         '''
-        Acyclicity constraint for the graph
-
-        Args: 
-            W (numpy.array): [n_variables, n_variables] graph
-
-        Output:
-            h (float) : restriction value 
-            G_h (float) : restriction gradient
+        Acyclicity constraint value for the graph and gradient
         '''
         #(Zheng et al. 2018 NOTEARS)
         # h(W) = tr[exp(W*W)] - n_variable
@@ -43,13 +40,7 @@ def notears_linear(X, h_tol, threshold, lambda1, c = 0.25, rho_max = 1e16, max_i
 
     def _loss(W):
         '''
-        Calculation of Loss
-        Args:
-            W (numpy.matrix) : [n_variables, n_variables] graph
-
-        Output:
-            loss (float) : Loss value
-            G_loss (float) : Loss gradient value
+        Calculation of loss value and gradient
         '''
         y = np.matmul(X, W)
         R = X - y
@@ -59,41 +50,49 @@ def notears_linear(X, h_tol, threshold, lambda1, c = 0.25, rho_max = 1e16, max_i
         return loss, G_loss
 
 
-    def _func(W, n):
+    def _func(W):
         '''
         Evaluate value and gradient of augmented Lagrangian
-        Args:
-            W (numpy.matrix) : [n_variables, n_variables] graph
-
-        Output:
-            Scoring function value
         '''
+        W  = _adj(W)
         loss, G_loss = _loss(W)
         h, G_h = _h(W)
         #F(w) = L(W) + rho/2 * h² + alpha * h + lambda1*|W|
-        f = loss + 0.5 * rho * h**2 + alpha * h + lambda1 * W.sum()
+        f = loss + 0.5 * rho * h**2 + alpha * h + lambda1 * np.abs(W).sum()
         #dF/dW = dL/dW(W) + rho * h * dh/dW + alpha * dh/dW
         G_f = G_loss + (rho * h + alpha) * G_h
-        return tfp.math.value_and_gradient(f, G_f)
+        return f, G_f
+
+
+    ########################
+    # Optimization process #
+    ########################
 
     n_samples, n_variables = X.shape
-    W_est, alpha, rho, h = np.zeros((n_variables, n_variables)), 0.0, 1.0, np.inf
-    for _ in range(max_iter):
+    W_est, alpha, rho, h = np.zeros([ 2 * n_variables**2]), 0.0, 1.0, np.inf
+    bnd = np.array([(0, 0) if i == j else (0, np.inf) for _ in range(2) for i in range(n_variables) for j in range(n_variables)])
+    for _ in range(max_iter): 
+        W_new, h_new = None, None
         while rho < rho_max:
-            W_est = tfp.optimizer.lbfgs_minimize(_func, W_est)
+            sol = sopt.minimize(_func, W_est, method = "L-BFGS-B", jac = True, bounds= bnd)
+            W_new = sol.x
             h_new, _ = _h(W_est)
-
-            #Ascent alpha
-            alpha = alpha + rho*h_new
 
             #Updating rho constraint parameter
             if h_new > h*c:
                 rho*=10
             else:
                 break
+        W_est, h = W_new, h_new
         
+        #Ascent alpha
+        alpha = alpha + rho*h_new
+
         #Verifying constraint tolerance
         if h <= h_tol or rho >= rho_max:
             break
-    W_est = W_est * (np.abs(W_est) > threshold)
+
+    #Applying threshold    
+    W_est = W_est * (W_est > threshold)
+    W_est = _adj(W_est)
     return W_est
